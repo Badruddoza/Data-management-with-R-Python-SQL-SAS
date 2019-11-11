f8bfc36b1db9b038ebbe26f59681aa610f7141c2
Syed Badruddoza <22337930+Badruddoza@users.noreply.github.com>
2019-11-11 14:39:20 -0800

    Add files via upload

---
+++ rows with maximum zeros.R
@@ -0,0 +1,19 @@
+rm(list=ls())
+
+p<-.5;num_row<-10;num_col<-10
+matrix<-matrix(c(0),num_row,num_col)
+for(j in 1:num_col){
+for(i in 1:num_row){
+ matrix[i,j]<-rbinom(1,1,p) 
+}
+}
+matrix
+
+sum<-matrix(c(0),num_row,1)
+for(i in 1:num_row){
+  sum[i]<-sum(matrix[i,])
+}
+res<-cbind.data.frame(zeros=num_col-sum,serial=rep(1:num_col))
+ii<-res[which(res[,1]==max(res[,1])),2]
+print("Row(s) with maximum zeros:")
+paste(ii)
